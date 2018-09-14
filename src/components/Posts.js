13dce90f5efebbe6a2e3512e8a8e5aa431ad050d
Steve Golden <sgolden2525@outlook.com>
2018-09-13 23:57:45 -0600

    Action switch and fetching Posts
    Adding switch to the the action.
    Working on fetching the posts within the actions file

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    }
-  }
 
   componentWillMount() {
     fetch('https://jsonplaceholder.typicode.com/posts')
