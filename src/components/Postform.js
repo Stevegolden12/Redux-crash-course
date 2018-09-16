01fe59054488deaf8f0205db754ab187d84324f2
Steve Golden <sgolden2525@outlook.com>
2018-09-15 23:56:10 -0600

    Added React extension link and postActions.js code
    Added code to link up React extension
    Added code in the postActions.js

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -20,19 +20,14 @@ class PostForm extends Component {
 
   onSubmit(e) {
     e.preventDefault();
+
     const post = {
       title: this.state.title,
       body: this.state.body
     }
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-      .then(res => res.json())
-      .then(data => console.log(data));
+
+    //Call Action
+
   }
 
   render() {
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/postActions';
 
@@ -10,7 +11,7 @@ class Posts extends Component {
 
 
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -25,9 +26,13 @@ class Posts extends Component {
   }
 }
 
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+}
+
 const mapStateToProps = state => ({
   posts: state.posts.items
 });
-};
 
-export default connect(null, { fetchPosts })(Posts);
+export default connect(mapStateToProps, { fetchPosts })(Posts);
